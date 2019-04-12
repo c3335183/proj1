e2018f4802920aa44c0e0ee6ad6f030b06095021
Jackson Dainer <jackson.dainer@uon.edu.au>
2019-04-12 03:21:49 +0000

    substitution cipher encrypt (still have to figure out how to use spaces)

--- main.c
+++ main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 
-int main() {
+/* Encryption system algebraic algorithms:
+    En(x) = (x+n)(mod 26)
+    Dn(x) = (x-n)(mod 26)
+*/
 
-printf("test");
+
+void caesar (char cipher[], int shift);
+
+int main () {
+
+  char cipher[50];
+  int shift;
+
+  printf("Plaintext (Caps only): ");
+  scanf("%s", cipher);
+
+  printf("Shift: ");
+  scanf("%d", &shift);
+
+  caesar (cipher, shift);
+
   return 0;
+}
+
+void caesar (char cipher[], int shift) {
+
+  for(int i=0; cipher[i] != '\0'; i++)
+    cipher[i] = 65 + (cipher[i]-65+shift)%26;
+  printf("Cipher text: %s\n", cipher);
 }
